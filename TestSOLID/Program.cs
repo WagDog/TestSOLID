dd00ae8ffbe3012b1146eae4be6b15e7389428a7
WagDog <paulwagstaff66@gmail.com>
2016-11-02 14:07:33 +0000

    Initial State

---
+++ TestSOLID/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "SOLID (Refer to Section 4). SOLID is an acronym for the following:-\n\n";
+            s += "S - Single Responsibility Principal\n";
+            s += "\tThere should never be more than one reason for a class change.\n";
+            s += "\tA class should only have one job to perform.\n";
+            s += "\tBy doing the above, classes become smaller, cleaner,\n\tsimpler to understand and maintain.\n";
+            s += "\n\nPress any key to continue.....";
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            s = "O - Open / Closed Principal\n";
+            s += "\tOpen to extension. New functionality or requirements without editing the existing code.\n";
+            s += "\tClosed to modification, except for bug corrections.\n";
+            s += "\tUse interfaces.\n";
+            s += "\n\nPress any key to continue.....";
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            s = "L - Liskov Substitution Principal\n";
+            s += "\tUse Derived Classes without knowing it.\n";
+            s += "\tSubclasses must operate in the same way as their base class.\n";
+            s += "\n\nPress any key to continue.....";
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            s = "I - Interface Segregation Principal\n";
+            s += "\tOpen to extension. New functionality or requirements without editing the existing code.\n";
+            s += "\tClosed to modification, except for bug corrections.\n";
+            s += "\tUse interfaces.\n";
+            s += "\n\nPress any key to continue.....";
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            s = "D - Dependancy Inversion Principal\n";
+            s += "\tHigh level modules should not depend on low level modules, both should depend on abstractions.\n";
+            s += "\tClients should not be forced to depend upon interfaces they do not use.\n";
+            s += "\tDIP removes direct dependencies between classes.\n";
+            s += "\tClasses become loosely coupled, making it easier to substitute alternative implementations.\n";
+            s += "\n\nPress any key to continue.....";
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestSOLID/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestSOLID")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSOLID")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64344578-7e60-4473-bbaa-c72d55f8a295")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
